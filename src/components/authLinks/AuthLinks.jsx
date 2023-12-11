d5180e955059237e829504560920422f1640fd88
Yingshi Liu <serendipper.liu@gmail.com>
2023-12-11 01:06:04 -0800

    style login button w/ simple auth logic

--- src/components/authLinks/AuthLinks.jsx
+++ src/components/authLinks/AuthLinks.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import styles from './authLinks.module.css';
+import Link from 'next/link';
 
 export default function AuthLinks() {
+  const status = "notauthenticated";
+
   return (
-    <div className={styles.container}>
-      AuthLinks
-    </div>
+    <>
+        {status === "notauthenticated"? (
+            <Link href="/login">Login</Link>
+        ) : (
+            <>
+            <Link href="/write">Write</Link>
+            <span className={styles.link}>Logout</span>
+            </>
+        )}
+    </>
   )
 }
