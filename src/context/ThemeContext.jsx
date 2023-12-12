028863c44b6adc1665ae1266b6050330b445963a
Yingshi Liu <serendipper.liu@gmail.com>
2023-12-11 21:51:37 -0800

    set up ThemeContextProvider

---
+++ src/context/ThemeContext.jsx
@@ -0,0 +1,26 @@
+"use client"
+
+import { createContext, useState } from 'react';
+
+export const ThemeContext = createContext()
+
+/** Next.js is using server side rendering by default, but we're trying to use browser 
+ * local storage, but in our server there is no browser, during the transition, it will
+ * cause some problems, so we need to check the typeof window.
+  */
+const getFromLocalStorage = () => {
+    if (typeof window !== undefined ) {
+        const value = localStorage.getItem("theme");
+        return value || "light";
+    }
+
+}
+
+export const ThemeContextProvider = ({ children }) => {
+    const [theme, setTheme ] = useState(() => {
+        return getFromLocalStorage();
+    });
+    return <ThemeContext.Provider>
+        {children}
+    </ThemeContext.Provider>
+}
