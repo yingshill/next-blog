03ba839a3f92b86cfae1b14b01a9771021a60243
Yingshi Liu <serendipper.liu@gmail.com>
2023-12-26 01:42:51 -0800

    Improved sign-in page w/ status updating and rerouting

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -2,11 +2,24 @@
 import React from 'react'
 import styles from "./loginPage.module.css";
 import { signIn, useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
 
 export default function LoginPage() {
   const { data, status } = useSession();
   console.log("data: ", data);
   console.log("status: ", status);
+
+  if (status === "loading") {
+    return <div className={styles.loading}>
+        Loading...
+    </div>
+  }
+
+  const router = useRouter();
+  if (status === "authenticated") {
+    router.push("/");
+  }
+
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
