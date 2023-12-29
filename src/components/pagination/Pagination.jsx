dc49cfe588e6bff0e2b2db38c2ee28f80d4b447e
Yingshi Liu <serendipper.liu@gmail.com>
2023-12-28 16:22:45 -0800

    Updated Pagination w/ page status

--- src/components/pagination/Pagination.jsx
+++ src/components/pagination/Pagination.jsx
@@ -1,11 +1,24 @@
+"use client"
 import React from 'react'
 import styles from './pagination.module.css'
+import { useRouter } from 'next/navigation'
 
-export default function Pagination() {
+export default function Pagination({page, hasPrev, hasNext}) {
+    const router = useRouter();
   return (
     <div className={styles.container}>
-      <button className={styles.button}>Previous</button>
-      <button className={styles.button}>Next</button>
+      <button 
+        className={styles.button} 
+        disabled={!hasPrev}
+        onClick={()=> router.push(`?page=${page-1}`)}>
+        Previous
+      </button>
+      <button 
+        className={styles.button}
+        disabled={!hasNext}
+        onClick={()=> router.push(`?page=${page+1}`)}>
+        Next
+      </button>
     </div>
   )
 }
